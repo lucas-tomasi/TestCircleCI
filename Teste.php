b155b3e7798d32814e8ddd340e87fdeba56e7bc4
Lucas Tomasi <lcstomasi@gmail.com>
2018-06-09 20:22:15 -0300

    Add test

--- Teste.php
+++ Teste.php
@@ -11,4 +11,9 @@ class Teste extends TestCase
 	{
 		return $this->assertEquals(Functions::sum(1,1), 2);
 	}
+
+	public function testeSoma2()
+	{
+		return $this->assertEquals(Functions::sum(10,1), 11);
+	}
 }
